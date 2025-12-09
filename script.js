ea69941fd68c1e5fcf184a60b4b34242f5699b6a
salahABOU03 <you@example.com>
2025-12-09 12:19:15 +0000

    fix navbarr

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ hamburger.addEventListener("click", () => {
 hamburger.addEventListener("touchstart", () => {
     navLinks.classList.toggle("active");
 });
+
+// Close menu when a link is clicked
+const navItems = document.querySelectorAll(".nav-links a");
+navItems.forEach(item => {
+    item.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
